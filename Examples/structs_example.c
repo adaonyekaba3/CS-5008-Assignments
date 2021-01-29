9383ba2760665e32549c154461e63490855ddc7e
Adaobi Onyekaba <adaonyekaba@login-students.ccs.neu.edu>
2021-01-29 14:35:51 -0500

    This is first structs example.

---
+++ Examples/structs_example.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+ 
+//This is a structs example - UNION OR CLASSES DATA TYPES
+
+struct student{
+	int id;
+	int age;
+	double creditHours;
+
+};
+
+int main(){
+	struct student s1;
+
+	return 0;
+}
