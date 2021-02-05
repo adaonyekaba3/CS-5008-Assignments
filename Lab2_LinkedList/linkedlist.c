1c6b1e079e97757b952908aefa44c704a23d9c7a
Adaobi Onyekaba <adaonyekaba@login-students.ccs.neu.edu>
2021-02-04 19:23:14 -0500

    updated linked list lab file.

--- Lab2_LinkedList/linkedlist.c
+++ Lab2_LinkedList/linkedlist.c
@@ -4,15 +4,58 @@
 #include <stdio.h>
 #include <stdlib.h> // contains the functions free/malloc
 
-// Create your node_t type here
+// Create your node_t type here - STRUCT.
+typedef struct node{
+	struct node* next; 
+	int wins;
+	int years;
 
+}node_t;
 
 // Write your functions here
 // There should be 1.) create_list 2.) print_list 3.) free_list
 // You may create as many helper functions as you like.
 
-int main(){
-  
- 
-    return 0;
+//Create the list.
+node_t* create_list(){
+	node_t* year2018 = malloc(sizeof(node_t));
+	node_t* year2017 = malloc(sizeof(node_t));
+	node_t* year2016 = malloc(sizeof(node_t));
+
+	// Set up data in each node 
+	year2018->years = 2018;
+	year2017->years = 2017; 
+	year2016->years = 2016; 
+	year2018->wins = 108;
+	year2017->wins = 93; 
+	year2016->wins = 92; 
+
+	// Link the list. 
+	year2018->next = year2017;
+	year2017->next = year2016;
+	year2016->next = NULL;
+	
+	return year2018;
 }
+
+void print_list(node_t* head){
+
+}
+
+int main(){
+	node_t* redsox = NULL;
+	redsox = create_list();
+
+	// iterate
+	// point to head of list -> redsox.
+	node_t* itr = redsox;
+	while( itr != NULL){
+	}
+		printf("wins in %d are %d\n", itr->years, itr-> wins);
+	
+		itr = itr->next;
+
+	// free our memory	
+	return 0;
+}	  
+ 
