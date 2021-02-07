50e1aebfb9473d328ee0feca6611857fc0cd8209
Adaobi Onyekaba <adaonyekaba@login-students.ccs.neu.edu>
2021-02-06 22:28:17 -0500

    This is updated stack test file.

--- Assignment2_C_Data_Structures/part2/stack_test.c
+++ Assignment2_C_Data_Structures/part2/stack_test.c
@@ -19,11 +19,67 @@
 // and provide the path to this file which is within
 // our current directory.
 
+// Print Elements of the stack.
+void printElements(stack_t* s){
+	int i;
+	if (stack_empty(s)){
+		printf("The stack is empty! \n");
+	}
+	else
+	{
+		for (int i = 0; i < s-> capacity; i++){
+		printf("%d\n");
+	}		
+}
+
+// Print Separator Line. 
+void printSeparator(){
+	printf("=========================================================================================");
+}
+
+
 
 // A sample test of your program
 // You can add as many unit tests as you like
 // We will be adding our own to test your program.
-int unitTest1(int status){
+void unitTest1(){
+	
+	printSeparator();
+	
+	stack_t* test1 = create_stack(1);
+	printf("Attempting to add %d\n", 1);
+	stack_enqueue(test1, 1);
+	printf("Removing: %d\n", stack_dequeue(test1));
+	
+	free_stack(test1);
+	printSeparator();
+}
+
+// Unit Test for overflow condition. 
+void unitTest_overflow(){
+	
+	printSeparator();
+	
+	printf("Creating a stack of capacity 32... \n");
+	stack_t* sampleStack = create_stack(1);
+	
+	printf("Size of stack is: %d\n", stack_size(sampleStack));
+	printf("Attempting to add. Addition attempt success: %d\n", stack_enqueue(sampleStack, 1));
+	printf("Attempting to add. Addition attempt success: %d\n", stack_enqueue(sampleStack, 2));
+	printf("Attempting to add. Addition attempt success: %d\n", stack_enqueue(sampleStack, 3));
+	printf("Attempting to add. Addition attempt success: %d\n", stack_enqueue(sampleStack, 4));
+	printf("Size of the stack is: %d\n", stack_size(sampleStack));
+
+	printf("Printing Elements... \n");
+	printElements(sampleStack);
+	
+	free_stack(sampleStack);
+	printSeparator();
+}
+
+// Unit Test for checking conditions when Stack is Full.
+
+*int unitTest1(int status){
     printf("=======Stack unitTest1=======\n");
     stack_t* test1 = create_stack(MAX_DEPTH);
     stack_enqueue(test1,1);
@@ -34,6 +90,7 @@ int unitTest1(int status){
     return 1;
 }
 
+/*
 int unitTest2(int status){
     printf("=======Stack unitTest2=======\n");
     stack_t* test1 = create_stack(MAX_DEPTH);
@@ -62,6 +119,7 @@ int unitTest3(int status){
     }
     for(i=0; i < 32; i++){
         stack_dequeue(test3);
+B
     }
 
     free_stack(test3);
@@ -100,6 +158,9 @@ int unitTest5(int status){
 }
 
 
+
+// 
+
 // TODO: Add tests here
 int (*unitTests[])(int)={
     unitTest1,
@@ -108,20 +169,27 @@ int (*unitTests[])(int)={
     unitTest4,
     unitTest5,
     NULL
-};
+}; */
 
 // ====================================================
 // ================== Program Entry ===================
 // ====================================================
 int main(){
 
-    unsigned int testsPassed = 0;
-    // List of Unit Tests to test your data structure
-    int counter =0;
-    while(unitTests[counter]!=NULL){
-        unitTests[counter](1);
-        counter++;
-    }
+	unsigned int testsPassed = 0;
+    	// List of Unit Tests to test your data structure
+	/*unitTest();
+	unitTest_overflow();
+	unitTest_underflow();
+	unitTest_stackFull();
+	unitTest_stackEmpty();
+	unitTest_stackInsertionAndDeletion();
+	unitTest_sizeCheckOnNull();
 
-    return 0;
+	print("Reached end of program. \n");
+
+    	*/
+	return 0;
 }
+
+
