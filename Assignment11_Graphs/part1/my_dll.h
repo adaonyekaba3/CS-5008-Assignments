3cfc13ca9b606e236250a14dcc22916c9fc0aa9a
adaonyekaba3 <adaonyekaba3@gmail.com>
2021-05-05 19:16:38 -0400

    final A11 done!

--- Assignment11_Graphs/part1/my_dll.h
+++ Assignment11_Graphs/part1/my_dll.h
@@ -51,8 +51,16 @@ dll_t* create_dll(){
 // Returns 1 if true (The DLL is completely empty)
 // Returns 0 if false (the DLL has at least one element enqueued)
 // Returns -1 if the dll is NULL.
-int dll_empty(dll_t* l){
-    return -1;
+int dll_empty(dll_t* l)
+{
+        if(l == NULL){
+                return -1;
+        }if(l->count == 0)
+        {
+                return 1;
+        }if(l->count >= 1){
+                return 0;
+        }
 }
 
 // push a new item to the front of the DLL ( before the first node in the list).
@@ -60,8 +68,32 @@ int dll_empty(dll_t* l){
 // Returns 0 on failure ( i.e. we couldn't allocate memory for the new node)
 // Returns -1 if DLL is NULL.
 // (i.e. the memory allocation for a new node failed).
-int dll_push_front(dll_t* l, void* item){
-    return -1;
+int dll_push_front(dll_t* l, void* item)
+{
+       if(l == NULL){
+               return -1;
+       }
+       node_t* newNode = (node_t*)malloc(sizeof(node_t));
+
+       if(newNode == 0){
+               free(newNode);
+               return 0;
+       }
+       // push a new item to the head
+       node_t* new_temp_node = l->head;
+       l->head = newNode;
+       newNode->data = item;
+       newNode->next = new_temp_node;
+       newNode->previous = NULL;
+
+       if(l->count != 0){
+               new_temp_node->previous = newNode;
+       }
+       if(l->count == 0){
+               l->tail = l->head;
+       }
+       l->count++;
+       return 1;
 }
 
 // push a new item to the end of the DLL (after the last node in the list).
@@ -69,26 +101,91 @@ int dll_push_front(dll_t* l, void* item){
 // Returns 0 on failure ( i.e. we couldn't allocate memory for the new node)
 // Returns -1 if DLL is NULL.
 // (i.e. the memory allocation for a new node failed).
-int dll_push_back(dll_t* l, void* item){
-        return -1;
+int dll_push_back(dll_t* l, void* item)
+{
+        if(l==NULL){
+                return -1;
+        }
+        node_t* newNode = (node_t*)malloc(sizeof(node_t));
+
+        if(newNode == 0){
+                free(newNode);
+                return 0;
+        }
+        // push setup for new item to the tail
+        node_t* new_temp_node = l->tail;
+        l->tail = newNode;
+        newNode->data = item;
+        newNode->next = NULL;
+        newNode->previous = new_temp_node;
+
+        if(l->count != 0){
+                new_temp_node->next = newNode;
+        }
+        if(l->count == 0){
+                l->head = l->tail;
+        }
+        l->count++;
+        return 1;
 }
 
 // Returns the first item in the DLL and also removes it from the list.
 // Returns 0 on failure, i.e. there is noting to pop from the list.
 // Returns a -1 if the DLL is NULL.
 // Assume no negative numbers in the list or the number zero.
-void* dll_pop_front(dll_t* t){
+void* dll_pop_front(dll_t* t)
+{
+        if(t == NULL){
+                return NULL;
+        }
+        if(dll_empty(t)){
+                free(t);
+                return 0;
+        }
+        char* del_front = t->head->data;
+        node_t* tempNode = t->head;
 
-        return -1; // Note: This line is a 'filler' so the code compiles.
+        if(t->count == 0){
+                t->head = NULL;
+                t->tail = NULL;
+        }else{
+                t->head = tempNode->next;
+                t->head->previous = NULL;
+        }
+        free(tempNode);
+
+        t->count--;
+        return del_front; // Note: This line is a 'filler' so the code compiles.
 }
 
 // Returns the last item in the DLL, and also removes it from the list.
 // Returns 0 on failure, i.e. there is noting to pop from the list.
 // Returns a -1 if the DLL is NULL.
 // Assume no negative numbers in the list or the number zero.
-void* dll_pop_back(dll_t* t){
+void* dll_pop_back(dll_t* t)
+{
+        if(t == NULL){
+               return NULL;
+        }
+        if(dll_empty(t)){
+                free(t);
+                return 0;
+        }
+        node_t* tempNode = t->tail->data;
 
-        return -1; // Note: This line is a 'filler' so the code compiles.
+        char* i = t->tail->data; // create new item
+
+        if(t->count == 1){
+                t->head = NULL;
+                t->tail = NULL;
+        }
+        else{
+                t->tail = tempNode->previous;
+                t->tail->next = NULL;
+        }
+        free(tempNode);
+        t->count--;
+        return i; // Note: This line is a 'filler' so the code compiles.
 }
 
 // Inserts a new node before the node at the specified position.
@@ -99,9 +196,37 @@ void* dll_pop_back(dll_t* t){
 //  * we tried to insert past the size of the list
 //   (inserting at the size should be equivalent as calling push_back).
 // Returns -1 if the list is NULL
-int dll_insert(dll_t* l, int pos, void* item){
-        
-        return -1; // Note: This line is a 'filler' so the code compiles.
+int dll_insert(dll_t* l, int pos, void* item)
+{
+        if(l == NULL){
+                return -1;
+        }
+        node_t* newNode = malloc(sizeof(node_t));
+
+        if(newNode == NULL || l->count <= 0 || pos < 0 || pos >= l->count){
+                free(newNode);
+                return 0;
+        }
+
+        node_t* new_pos_node = l->head;
+        int i = 0;
+        for(i = 0; i < pos; i++){
+                new_pos_node = new_pos_node->next;
+        }
+        newNode->data = item;
+        newNode->next = new_pos_node;
+
+        if(pos == 0){
+                new_pos_node->previous = NULL;
+                l->head = newNode;
+        }
+        else{
+                newNode->previous = new_pos_node->previous;
+                new_pos_node->previous->next = newNode;
+        }
+        new_pos_node->previous = newNode;
+        l->count++;
+        return 1; // Note: This line is a 'filler' so the code compiles.
 }
 
 // Returns the item at position pos starting at 0 ( 0 being the first item )
@@ -111,8 +236,24 @@ int dll_insert(dll_t* l, int pos, void* item){
 //  * we tried to get past the size of the list
 // Returns -1 if the list is NULL
 // Assume no negative numbers in the list or the number zero.
-void* dll_get(dll_t* l, int pos){
-        return NULL; // Note: This line is a 'filler' so the code compiles.
+void* dll_get(dll_t* l, int pos)
+{
+        if(l == NULL){
+                return NULL;
+        }
+        if(dll_empty(l) || pos < 0 || pos >= l->count){
+                free(l);
+                return 0;
+        }
+        else{
+                node_t* pos_node = l->head; // new temp node
+                int i; // initialized and declare an index node
+                for(i=0; i < pos; i++){
+                        pos_node = pos_node->next;
+                }
+                return pos_node->data;
+        }
+        // Note: This line is a 'filler' so the code compiles.
 }
 
 // Removes the item at position pos starting at 0 ( 0 being the first item )
@@ -121,27 +262,165 @@ void* dll_get(dll_t* l, int pos){
 //  * we tried to remove get past the size of the list
 // Returns NULL if the list is NULL
 // Assume no negative numbers in the list or the number zero.
-void* dll_remove(dll_t* l, int pos){
-        
-        return NULL; // Note: This line is a 'filler' so the code compiles.
+void* dll_remove(dll_t* l, int pos)
+{
+        if(l == NULL){
+                return NULL;
+        }
+        int cur = 0;
+        node_t* ptr = l->head;
+
+        if(pos < 0 || pos >= l->count || l->count == 0){
+                return 0;
+        }
+        else if(pos == 0){
+                return dll_pop_front;
+        }
+        if(pos == l->count-1){
+                return dll_pop_back;
+        }
+        while(cur != pos){
+                ptr = ptr->next;
+                cur++;
+        }
+                ptr->next->previous = ptr->previous;
+                free(ptr);
+                l->count--;
+                return 0;
+        // Note: This line is a 'filler' so the code compiles.
 }
 
 // DLL Size
 // Queries the current size of a DLL
 // Returns -1 if the DLL is NULL.
-int dll_size(dll_t* t){
-    
-        return -1; // Note: This line is a 'filler' so the code compiles.
+int dll_size(dll_t* t)
+{
+        if(t == NULL){
+                return -1;
+        }else{
+                return t->count;
+        }
+        // Note: This line is a 'filler' so the code compiles.
 }
 
 // Free DLL
 // Removes a DLL and all of its elements from memory.
 // This should be called before the proram terminates.
-void free_dll(dll_t* t){
-
+void free_dll(dll_t* t)
+{
+        node_t* temp_node;
+        while(t->head != NULL){
+                temp_node = t->head->next;
+                free(t->head);
+                t->head = temp_node; 
+        }
+        t->tail = NULL;
+        t->count = 0;
+        free(t); 
 }
 
+//Helper method
 
+int DLL_find(dll_t *l, void *data)
+{
 
+        if (l == NULL)
+        {
+                return -1;
+        }
+
+        node_t *iterator = l->head;
+        while (iterator != NULL)
+        {
+                if (iterator->data = data)
+                {
+                        return 1;
+                }
+                iterator = iterator->next;
+        }
+        return 0;
+}
+
+//Helper method
+int dll_remove_value(dll_t *l, void *item)
+{
+
+        if (l == NULL)
+        {
+                return -1;
+        }
+        node_t *iterator = l->head;
+
+        while (iterator != NULL)
+        {
+                if (iterator->data == item)
+                {
+                        iterator->previous->next = iterator->next;
+                        iterator->next->previous = iterator->previous;
+                        free(iterator);
+                        l->count -= 1;
+                        return 1;
+                }
+                iterator = iterator->next;
+        }
+        return 0;
+}
+
+//Helper method
+int find_helper_dll(dll_t *l, void *item)
+{
+
+        if (l == NULL)
+        {
+                return -1;
+        }
+
+        node_t *iterator = l->head;
+        while (iterator != NULL)
+        {
+                if (iterator->data == item)
+                {
+                        return 1;
+                }
+                iterator = iterator->next;
+        }
+        return 0;
+}
+
+//Remove Helper
+int dll_remove_helper(dll_t *l, void *value)
+{
+
+        if (l == NULL)
+        {
+                return -1;
+        }
+
+        node_t *iterator = l->head;
+        int i = 0;
+        for (i = 0; i < l->count; i++)
+        {
+                if (iterator->data == value)
+                {
+                        if (i == 0)
+                        {
+                                dll_pop_front(l);
+                        }
+                        else if (i == l->count - 1)
+                        {
+                                dll_pop_back(l);
+                        }
+                        else
+                        {
+                                iterator->previous->next = iterator->next;
+                                iterator->next->previous = iterator->previous;
+                                free(iterator);
+                                l->count--;
+                                return 1;
+                        }
+                        iterator = iterator->next;
+                }
+                return 0;
+        }
+}
 #endif
-
--- Assignment11_Graphs/part1/my_graph.h
+++ Assignment11_Graphs/part1/my_graph.h
@@ -44,8 +44,23 @@ graph_t* create_graph(){
 
 // Returns the node pointer if the node exists.
 // Returns NULL if the node doesn't exist or the graph is NULL
-graph_node_t* find_node( graph_t * g, int value){
+graph_node_t* find_node( graph_t * g, int value)
+{
+   if(g == NULL){
        return NULL;
+   }
+    node_t* iterator = g->nodes->head;
+
+    while (iterator != NULL)
+    {
+        graph_node_t* tempNode = iterator->data;
+
+        if(tempNode->data == value){
+            return tempNode;
+        }
+            iterator = iterator->next;
+    }
+    return NULL;
 }
 
 // Creates a graph node
@@ -86,7 +101,28 @@ int graph_add_node(graph_t* g, int value){
 int graph_remove_node(graph_t* g, int value){
     // The function removes the node from the graph along with any edges associated with it.
     // That is, this node would have to be removed from all the in and out neighbor's lists that countain it.
-    return -1;
+    if(g==NULL){
+        return -1;
+    }
+    graph_node_t* tempNode = find_node(g, value);
+
+    if(tempNode == 0){
+        return 0;
+    }
+    // iterator process
+    node_t* iterator = g->nodes->head;
+
+    while(iterator != NULL)
+    {
+        graph_node_t* tempNode = iterator->data;
+
+        dll_remove_value(tempNode->inNeighbors, tempNode);
+        dll_remove_value(tempNode->outNeighbors, tempNode);
+        iterator = iterator->next;
+    }
+    dll_remove_value(g->nodes, tempNode);
+    free(tempNode);
+    return 1;
 }
 
 // Returns 1 on success
@@ -97,7 +133,23 @@ int graph_add_edge(graph_t * g, int source, int destination){
     // Make sure you are not adding the same edge multiple times.
     // Make sure you modify the in and out neighbors appropriatelly. destination will be an out neighbor of source.
     // Source will be an in neighbor of destination.
-    return -1;
+    if(g == NULL){
+        return -1;
+    }
+    graph_node_t* node_source = find_node(g, source);
+    graph_node_t* node_destination = find_node(g, destination);
+
+    if(node_source == NULL){
+        return 0;
+    }
+    if(node_destination == NULL){
+        return 0;
+    }
+    dll_push_back(node_destination->inNeighbors, node_source);
+    dll_push_back(node_source->outNeighbors, node_destination);
+
+    g->numEdges++;
+    return 1;
 }
 
 // Returns 1 on success
@@ -107,57 +159,271 @@ int graph_remove_edge(graph_t * g, int source, int destination){
     //The function removes an edge from source to destination but not the other way.
     //Make sure you remove destination from the out neighbors of source.
     //Make sure you remove source from the in neighbors of destination.
-    return -1;
+    if(g == NULL){
+        return -1;
+    }
+    graph_node_t* node_source = find_node(g, source);
+    graph_node_t* node_destination = find_node(g, destination);
+
+    if(node_source == NULL){
+        return 0;
+    }
+    if(node_destination == NULL){
+        return 0;
+    }
+    dll_remove_helper(node_source->outNeighbors, node_destination);
+    dll_remove_helper(node_destination->inNeighbors, node_source);
+
+    g->numEdges--;
+    return 1;
 }
 
 // Returns 1 on success
 // Returns 0 on failure ( or if the source or destination nodes don't exist )
 // Returns -1 if the graph is NULL.
-int contains_edge( graph_t * g, int source, int destintaion){
-    return -1;
+int contains_edge( graph_t * g, int source, int destination){
+    if(g == NULL){
+        return -1;
+    }
+    if(find_node(g, source) == 0){
+        return 0;
+    }
+    if(find_node(g, destination == 0)){
+        return 0;
+    }
+    graph_node_t* temp_source_node = find_node(g, source);
+    graph_node_t* temp_dest_node = find_node(g, destination);
+
+    return DLL_find(temp_source_node->outNeighbors, temp_dest_node) && DLL_find(temp_dest_node->inNeighbors, temp_source_node);
 }
 
 // Returns dll_t* of all the in neighbors of this node.
 // Returns NULL if thte node doesn't exist or if the graph is NULL.
 dll_t* getInNeighbors( graph_t * g, int value ){
-    return NULL;
+    if(g == NULL){
+        return NULL;
+    }
+    graph_node_t* node = find_node(g,value);
+
+    if(node == NULL){
+        return NULL;
+    }
+    return node->inNeighbors;
 }
 
 // Returns the number of in neighbors of this node.
 // Returns -1 if the graph is NULL or the node doesn't exist.
 int getNumInNeighbors( graph_t * g, int value){
-    return -1;
+    if(g == NULL){
+        return -1;
+    }
+    graph_node_t* tempNode = find_node(g, value);
+
+    if(tempNode == NULL){
+        return -1;
+    }
+    return tempNode->inNeighbors->count;
 }
 
 // Returns dll_t* of all the out neighbors of this node.
 // Returns NULL if thte node doesn't exist or if the graph is NULL.
 dll_t* getOutNeighbors( graph_t * g, int value ){
-    return NULL;
+    if(g == NULL){
+        return NULL;
+    }
+    graph_node_t* tempNode = find_node(g, value);
+
+    if(tempNode == NULL){
+        return NULL;
+    }
+    return tempNode->outNeighbors;
 }
 
 // Returns the number of out neighbors of this node.
 // Returns -1 if the graph is NULL or the node doesn't exist.
 int getNumOutNeighbors( graph_t * g, int value){
-    return -1;
+    if(g == NULL){
+        return -1;
+    }
+    graph_node_t* tempNode = find_node(g,value);
+
+    if(tempNode == NULL){
+        return -1;
+    }
+    return tempNode->outNeighbors->count;
 }
 
 // Returns the number of nodes in the graph
 // Returns -1 if the graph is NULL.
 int graph_num_nodes(graph_t* g){
-    return 0;
+    if(g == NULL){
+        return -1;
+    }
+    return g->numNodes;
 }
 
 // Returns the number of edges in the graph,
 // Returns -1 on if the graph is NULL
 int graph_num_edges(graph_t* g){
-    return 0;
+    if(g == NULL){
+        return -1;
+    }
+    return g->numEdges;
+}
+
+// Returns 1 if reachable
+// returns 0 if not reachable
+// returns -1 if the graph is NULL or if either of the nodes doesn't exist.
+int is_reachable(graph_t* g, int source, int destination){
+    // implement using BFS technique
+    // Make sure you don't get stuck into an infinite loop.
+    if(g == NULL){
+        return -1;
+    }
+    
+    if(source == destination){
+        return 1;
+    }
+
+    int found = 0;
+    int visits = 0;
+    
+    dll_t* gDLL = create_dll(graph_num_nodes(g));
+    node_t* iterator = g->nodes->head;
+    graph_node_t* current_node = NULL;
+    graph_node_t* neighbor_node = current_node->outNeighbors;
+    dll_push_front(gDLL, iterator->data);
+
+    while(dll_empty(gDLL) != NULL){
+        visits = dll_pop_back(gDLL);
+
+        if(visits == destination){
+            found = 1;
+            break;
+        }
+        current_node = find_node(g, visits);
+        neighbor_node = current_node->outNeighbors;
+
+        while (neighbor_node != NULL)
+        {
+            if (current_node->outNeighbors != NULL)
+            {
+                dll_pop_back(gDLL);
+            }
+            neighbor_node = neighbor_node->data;
+        }
+    }
+    free_dll(gDLL);
+}
+
+// Returns 1 if the graph has a cycle.
+// Returns 0 if the graph doesn't have a cycle.
+// Returns -1 if the graph is NULL or if either of the nodes doesn't exist.
+int has_cycle(graph_t* g){
+    // implement using DFS
+    // Make sure you don't get stuck in an infinite loop
+    if(g==NULL){
+        return -1;
+    }
+    int found = 0;
+    int visits = 0;
+
+    dll_t* gDLL = create_dll(graph_num_nodes(g));
+    node_t* iterator = g->nodes->head;
+    graph_node_t* current_node = NULL;
+    graph_node_t* neigh_node;
+    
+    current_node->outNeighbors;
+    dll_push_front(gDLL, iterator->data);
+
+    while (dll_empty(gDLL) != NULL)
+    {
+        visits = dll_pop_back(gDLL);
+
+        if(g == visits){
+            found = 1;
+            break;
+        }
+        current_node = find_node(g, visits);
+        neigh_node = current_node->outNeighbors;
+        
+        while (neigh_node != NULL)
+        {
+            if (current_node->outNeighbors != NULL)
+            {
+                dll_pop_back(gDLL);
+            }
+            neigh_node = neigh_node->data;
+        }
+    }
+    free_dll(gDLL);
+}
+// Prints all the nodes from source to destination (including the source and destination).
+// i.e. print_path(1,5) => 1 2 3 4 5
+// otherwise it prints "path does not exit" or if source or destination nodes do not exist or if the graph is NULL.
+void print_path(graph_t* g, int source, int destination)
+{
+    if (g == NULL)
+    {
+        return -1;
+    }
+    if (is_reachable(g, source, destination) == NULL)
+    {
+        printf("Path does not exist\n", source);
+    }
+    if (source == destination)
+    {
+        printf("Path does not exist\n", source);
+    }
+    if (is_reachable(g, source, destination) == 0)
+    {
+        printf("Path does not exist\n", source);
+    }
+    if (source != destination)
+    {
+        node_t* tempNode = g->nodes->head;
+        graph_node_t* newNode = tempNode->data;
+
+        while (newNode != NULL)
+        {
+            if (newNode->data == destination)
+            {
+                printf("%d\n", destination);
+            }
+            if (is_reachable(g, newNode->data, destination) == 0)
+            {
+                break;
+            }
+
+        }
+        if (newNode != NULL)
+        {
+            print_path(g, newNode->data, destination);
+        }
+    }
 }
 // Free graph
 // Removes a graph and ALL of its elements from memory.
 // This should be called before the program terminates.
 // Make sure you free all the dll's too.
-void free_graph(graph_t* g){
+void free_graph(graph_t* g)
+{
+    if (g == NULL)
+    {
+        return;
+    }
+    node_t* tempNode = g->nodes->head;
 
+    while (tempNode != NULL)
+    {
+        graph_node_t* graphNode = (graph_node_t*)tempNode->data;
+        free_dll(graphNode->inNeighbors);
+        free_dll(graphNode->outNeighbors);
+        free(graphNode);
+        tempNode = tempNode->next;
+    }
+    free_dll(g->nodes);
+    free(g);
 }
 
 
